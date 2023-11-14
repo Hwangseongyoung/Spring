86d98bc86f72adf503156df074499f3bcedb7010
Hwangseongyoung <dfghukl1333@naver.com>
2023-11-14 16:44:37 +0900

    1114

---
+++ spring수업/tut01(링크이동 복습)/src/main/java/com/example/tut01/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.example.tut01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/admin")
+    public String getAdmin() {
+        return "admin/admin.html";
+    }
+    @GetMapping("/employees")
+    public String getEmp() {
+        return "admin/employees.html";
+    }
+    @GetMapping("/article")
+    public String getArticle() {
+        return "admin/article.html";
+    }
+    @GetMapping("/site")
+    public String getSite() {
+        return "admin/site.html";
+    }
+}
