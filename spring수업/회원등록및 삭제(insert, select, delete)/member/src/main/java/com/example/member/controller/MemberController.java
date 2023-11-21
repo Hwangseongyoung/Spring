1d41b39aaefd04ade92bc87dcc8686a2f54bf4e9
Hwangseongyoung <dfghukl1333@naver.com>
2023-11-21 16:35:18 +0900

    1121

---
+++ spring수업/회원등록및 삭제(insert, select, delete)/member/src/main/java/com/example/member/controller/MemberController.java
@@ -0,0 +1,55 @@
+package com.example.member.controller;
+
+import com.example.member.dto.MemberDto;
+import com.example.member.mappers.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @Autowired
+    MemberMapper memberMapper;
+
+    @GetMapping("")
+    public String getMemberList(Model model, @RequestParam(defaultValue = "") String searchType, @RequestParam(defaultValue = "") String words) {
+        String queryString = "";
+        if(searchType.equals("userid")) {
+            queryString = "where userid = '"+words+"'";
+        }else if(searchType.equals("username")) {
+            queryString = "where username = '"+words+"'";
+        }else {
+            queryString = "";
+        }
+//        System.out.println(queryString);
+
+        model.addAttribute("mem", memberMapper.getMemberList(queryString));
+        model.addAttribute("cnt", memberMapper.getMemberCount(queryString));
+        return "member/list.html";
+    }
+
+    @GetMapping("/insert")
+    public String getInsert() {
+        return "member/insert.html";
+    }
+
+    @PostMapping("/insert")
+    public String setInsert(@ModelAttribute MemberDto memberDto, RedirectAttributes ra) {
+        memberMapper.setInsert(memberDto);
+//        post 타입 : 주소에 드러나지 않음
+//        model.attribute -> get 방식
+        ra.addFlashAttribute("msg", "회원가입이 완료되었습니다.");
+        return "redirect:/member";
+    }
+
+    @GetMapping("/delete")
+    public String deleteMember(@RequestParam int id, RedirectAttributes ra) {
+        memberMapper.deleteMember(id);
+        ra.addFlashAttribute("msg", "회원정보가 삭제되었습니다.");
+        return "redirect:/member";
+    }
+}
---
+++ spring수업/회원등록및 삭제(insert, select, delete)/member/src/main/java/com/example/member/mappers/MemberMapper.java
@@ -0,0 +1,24 @@
+package com.example.member.mappers;
+
+import com.example.member.dto.MemberDto;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface MemberMapper {
+    @Insert("insert into member values(null, #{userid}, #{passwd}, #{username}, now())")
+    public void setInsert(MemberDto memberDto);
+
+    @Select("select * from member ${queryString} order by id desc")
+    public List<MemberDto> getMemberList(String queryString);
+
+    @Select("select count(*) from member ${queryString}")
+    public int getMemberCount(String queryString);
+
+    @Delete("delete from member where id = #{id}")
+    public void deleteMember(int id);
+}
